556f65ed84286d400d97c724694063ac92729439
jiteshsrivastava312 <155711775+jiteshsrivastava312@users.noreply.github.com>
2025-02-05 12:20:51 +0530

    Update oct.py

--- oct.py
+++ oct.py
@@ -16,8 +16,8 @@ db_config = {
 query = """
 SELECT * 
 FROM nr_conn_cdr 
-WHERE recordentrydate >= '2021-10-01' 
-AND recordentrydate <= '2021-10-30';
+WHERE recordentrydate >= '2020-10-01' 
+AND recordentrydate <= '2020-10-30';
 """
 try:
     # Connect to PostgreSQL and fetch data
@@ -32,7 +32,7 @@ try:
 
     # Create the dynamic filename with current date and time
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_path = f"/tmp/cdr_report_202110{current_time}.csv"
+    file_path = f"/tmp/cdr_report_202010{current_time}.csv"
 
     # Save the CSV file locally in /tmp/
     with open(file_path, 'w') as f:
