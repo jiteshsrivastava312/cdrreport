90bb67e74b0e0aa7176a78f17d4cc8b4a54ef766
jiteshsrivastava312 <155711775+jiteshsrivastava312@users.noreply.github.com>
2025-02-16 18:52:23 +0530

    Update ngucc.py

--- ngucc.py
+++ ngucc.py
@@ -16,8 +16,8 @@ db_config = {
 query = """
 SELECT * 
 FROM nr_conn_cdr 
-WHERE recordentrydate >= '2022-01-01' 
-AND recordentrydate <= '2022-01-31';
+WHERE recordentrydate >= '2019-01-01' 
+AND recordentrydate <= '2019-01-31';
 """
 try:
     # Connect to PostgreSQL and fetch data
@@ -32,7 +32,7 @@ try:
 
     # Create the dynamic filename with current date and time
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_path = f"/tmp/cdr_report_202201{current_time}.csv"
+    file_path = f"/tmp/cdr_report_201901{current_time}.csv"
 
     # Save the CSV file locally in /tmp/
     with open(file_path, 'w') as f:
